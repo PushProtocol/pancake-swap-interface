df48bea6314d688a3ab727af8e739e669d507521
PushProtocol <81375319+PushProtocol@users.noreply.github.com>
2021-04-01 20:18:39 +0500

    Update SwapRoute.tsx

--- src/components/swap/SwapRoute.tsx
+++ src/components/swap/SwapRoute.tsx
@@ -1,7 +1,7 @@
 import { Trade } from '@pancakeswap-libs/sdk'
 import React, { Fragment, memo, useContext } from 'react'
 import { ChevronRight } from 'react-feather'
-import { Flex, Text } from 'pushswap-layout'
+import { Flex, Text } from '@malikfarhan/design2'
 import { ThemeContext } from 'styled-components'
 import CurrencyLogo from '../CurrencyLogo'
 
