2144deaae0fe7a640ed1e5791209bd801586cfda
PushProtocol <81375319+PushProtocol@users.noreply.github.com>
2021-04-01 19:24:01 +0500

    Update SettingsModal.tsx

--- src/components/PageHeader/SettingsModal.tsx
+++ src/components/PageHeader/SettingsModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Modal } from '@pancakeswap-libs/uikit'
+import { Modal } from 'pushswap-layout'
 import SlippageToleranceSetting from './SlippageToleranceSetting'
 import TransactionDeadlineSetting from './TransactionDeadlineSetting'
 
