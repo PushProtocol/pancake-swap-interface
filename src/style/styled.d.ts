974e8538dd0f7888949f3c521780c676ca69c7a3
PushProtocol <81375319+PushProtocol@users.noreply.github.com>
2021-04-01 20:10:31 +0500

    Update styled.d.ts

--- src/style/styled.d.ts
+++ src/style/styled.d.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-unresolved
-import { PancakeTheme } from 'pushswap-layout/dist/theme'
+import { PancakeTheme } from '@malikfarhan/design2/dist/theme'
 
 declare module 'styled-components' {
   /* eslint-disable @typescript-eslint/no-empty-interface */
